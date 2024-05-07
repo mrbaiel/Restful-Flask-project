4246a3871a4a4a566917f55e2e36a259ebe735e7
Baiel <jyrgalbekuulubaiel@gmail.com>
2024-05-07 19:42:07 +0700

    added get, delete, post methods

--- local.py
+++ local.py
@@ -1,4 +1,6 @@
 import requests as req
 
-response = req.get("127.0.0.1:5000")
+#response = req.get("http://127.0.0.1:5001/api/index/3")
+#response = req.delete("http://127.0.0.1:5001/api/index/3")
+response = req.post("http://127.0.0.1:5001/api/index/4", {"audi": "a3", "engine_capacity": "2"})
 print(response.json())
--- main.py
+++ main.py
@@ -1,8 +1,49 @@
 from flask import Flask
-from flask_restful import Api, Resource
-
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
+api = Api()
+
+datas = {
+    1: {"audi": "q7", "eng_capacity": 4.7},
+    2: {"audi": "rs7", "eng_capacity": 3},
+    3: {"audi": "a8", "eng_capacity": 2}
+}
+
+
+def checking_num(car_number):
+    if car_number in datas:
+        return True
+    return False
+
+
+def not_in_date(car_num) -> str:
+    return f"{car_num} is not in dict"
+
+
+class Index(Resource):
+    def get(self, car_num):
+        if checking_num(car_num):
+            return datas[car_num]
+        return not_in_date(car_num)
+
+    def delete(self, car_num):
+        if checking_num(car_num):
+            del datas[car_num]
+            return datas
+        return not_in_date(car_num)
+
+    def post(self, new_car_num):
+        parser = reqparse.RequestParser()
+        parser.add_argument("audi", type=str)
+        parser.add_argument("engine_capacity", type=float)
+        datas[new_car_num] = parser.parse_args()
+        return datas
+
+
+
+api.add_resource(Index, '/api/index/<int:car_num>')
+api.init_app(app)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=5001, host='127.0.0.1')
